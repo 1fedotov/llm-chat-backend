8c1ace8e6873a9a755b3cf46f1bc3abf36fd3774
Ilya Fedotov <161419264+1fedotov@users.noreply.github.com>
2026-03-17 14:36:06 +0800

    Update config.ts

--- apps/server/src/config/config.ts
+++ apps/server/src/config/config.ts
@@ -13,7 +13,7 @@ interface Config {
 const config: Config = {
   port: Number(process.env.PORT) || 5000,
   db_uri: process.env.MONGODB_URL || "mongodb://localhost:27017/llm-chat-backend",
-  llm_model: process.env.OLLAMA_MODEL || "gemma3:270b",
+  llm_model: process.env.OLLAMA_MODEL || "gemma3:270m",
   llm_url: process.env.OLLAMA_URL || "http://ollama:11434",
 };
 
